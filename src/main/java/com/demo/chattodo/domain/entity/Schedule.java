fa602926cc456eaf0fdea853d851d6c89df29cbb
swandevson <shai4956@gmail.com>
2024-09-05 15:14:34 +0900

    :sparkles: schedule 엔티티 생성

---
+++ src/main/java/com/demo/chattodo/domain/entity/Schedule.java
@@ -0,0 +1,22 @@
+package com.demo.chattodo.domain.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Schedule {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String userId;
+	private String title;
+	private LocalDateTime startDateTime;
+	private LocalDateTime endDateTime;
+	private String location;
+}
