b8ffc30fc402c81f7f738916418ecae2a2e64a2e
swandevson <shai4956@gmail.com>
2024-09-05 09:24:56 +0900

    init spring boot project

---
+++ src/main/java/com/demo/chattodo/ChattodoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.chattodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/chattodo/ChattodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.chattodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
